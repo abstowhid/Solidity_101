e3b0d1c82bf28e154260090ff96da657aa13e08d
Towhid <blatowhid@gmail.com>
2025-02-23 21:06:03 +0600

    addedso

--- 020_inheritense.sol
+++ 020_inheritense.sol
@@ -6,17 +6,18 @@
 pragma solidity ^0.8.0;
 
 contract ErrorHandlingDemo {
+    address public something;
 uint public number=59087;
-    function whats() public  view virtual   returns (address){
-        return (msg.sender);
+    function whats() public  virtual   returns (address){
+        return something;
     }
 
 }
 contract second is ErrorHandlingDemo{
     function get() public view returns (uint){
     return number; }
-    function whats() public   view override returns (address){
-        return (msg.sender);
+    function whats() public    override returns (address){
+        return (something=msg.sender);
     }
 
 }
