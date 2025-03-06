61b5d2b4d74ef714ad731bc53c7629b18782c207
Towhid <blatowhid@gmail.com>
2025-03-06 20:22:31 +0600

    Update 027_0_struct.sol

--- 027_0_struct.sol
+++ 027_0_struct.sol
@@ -1,43 +1,33 @@
-//declare
-//implement in a variable (as array,mapping,parameter)
-//set and get values like as same as array & mapping (just in place of value , use structName(values);
-
+//1.declare
+//2.implement in a variable (as array,mapping,parameter)
+//3.set and get values like as same as array & mapping (just in place of value , use structName(values)
+//
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-contract First {
-//declare  struct with a name
-struct Task{
-    //declare=>how many and which data types & variable i want to use
-string name;
-string level;
-bool completed;
-}
-//1.usage as a state variable:
-Task[] public allTask; //so,allTask is the array variable that store 'Task type data' in array format . every index of array will have a new struct as i push value by index reference
-//more ways :
- mapping (uint=>Task) tasks; //so, every key will have a single struct(Task here)
-// Task public tsks;   //Task is the data type and tsks is variable that store Task type data(including all childs inside Task) (only one Task)
-// mapping (uint=>Task[]) task; //so, each key will have multiple Task(each key will have an array and the array can have it's own index and indexwise more Task)
-//more ways in next file<<
-
-
-//example: get and set value in struct( for  array)
-        function setAllTask(string memory _name, string memory _level,bool _completed) external   {
-            //arr.push(values);  =>values=> Task(values)
-            allTask.push(Task(_name,_level,_completed));   // store one by one struct(Task) by index number & imagine the index number is the task number
-        }
-    function check(uint index) external   view returns(bool,string memory) {
-        return (allTask[index].completed,allTask[index].name);
+contract EasyGetSetStruct{
+    // declare data type
+    struct Person{
+        string name;
+        uint age;
+        bool alive;
     }
-//example: get and set value in struct( for mapping)
+    //store
+    Person public person1;
+    //set-1
+    function set1(string memory _name,uint _age,bool _alive) public {
+        person1 = Person(_name,_age,_alive);
+    }
+    //set-2
+    function set2(string memory _name,uint _age)public {
+        person1.name=_name;
+        person1.age=_age;
+    }
+    
+    //get
+    function get1()public view returns (string memory,uint,bool){
+        return (person1.name,person1.age,person1.alive);
+    }
+}
 
-function setm(uint ind,string memory f,string memory g, bool d) public {
-//tasks[key]=values;  =>values=> Task(values)
-tasks[ind]=Task(f,g,d);
-}
-function getm(uint ind)public  view  returns (string memory,bool){
-    return (tasks[ind].name,tasks[ind].completed);
-}
 
-}
