a8f5c0df5e8160fe8ececc17997c406eeb53d56f
Towhid <blatowhid@gmail.com>
2025-03-26 17:01:39 +0600

    i donno

--- 007_Variable.sol
+++ 007_Variable.sol
@@ -1,2 +1,7 @@
 //state variable
 //function variable
+
+//1.state
+//In Solidity, state-changing functions do not return values when called via a transaction (because transactions do not return data; they execute changes in the blockchain). {that means when i will try to change the state variable and also try to return a while in a transaction the return don't work.}
+
+//Pure , View , nothing =>depends on state variable
